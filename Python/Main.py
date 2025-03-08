a06b0e428115a62b7756a4277b12ab63205aa6cf
Pol <Tihersarc35@gmail.com>
2025-03-09 00:09:16 +0100

    First Commit

---
+++ Python/Main.py
@@ -0,0 +1,6 @@
+from song import *
+
+s1 = Song("Silk In The Strings", "Spiritbox", "Blah, blah yeah")
+
+print(s1.artist)
+
---
+++ Python/song.py
@@ -0,0 +1,6 @@
+
+class Song:
+	def __init__(self, name, artist, lyrics):
+		self.name = name
+		self.artist = artist
+		self.lyrics = lyrics
